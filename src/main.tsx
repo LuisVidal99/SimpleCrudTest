3328c26eca7f05914441cd271c79c55df3d6b218
Luis Alberto <luis.alberto@dspot.pl.com>
2023-04-27 12:05:42 -0400

    add main file

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import Provider from './providers';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+	<Provider>
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+		<ReactQueryDevtools initialIsOpen={false} />
+	</Provider>
+);
