0cdc32bf9f3f337d38d34f02bf09b7f0c3f24cf7
Luis Alberto <luis.alberto@dspot.pl.com>
2023-04-29 16:50:19 -0400

    add task post hoocks

--- src/hooks/post-task.hook.ts
+++ src/hooks/post-task.hook.ts
@@ -1,5 +1,5 @@
 import { useMutation } from '@tanstack/react-query';
-import { postTask } from '../components/TasksComponent/Services/TaskApi';
+import { postTask } from '../components/TasksComponent/Services/task.service';
 
 export default function usePostTask() {
 	const { mutate: post } = useMutation(postTask, {
